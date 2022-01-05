a3a5179a5dedb5f1ba0728d8ae12e6027b04751d
jake.k <jake.k@hpcnt.com>
2022-01-05 20:27:05 +0900

    Initial Commit

---
+++ ExPhotoPermission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExPhotoPermission
+//
+//  Created by Jake.K on 2022/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
